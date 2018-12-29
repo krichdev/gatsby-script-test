9ff5751a7777cb4f24daa83a765a3c06dc96829d
krichdev <kgrichardson2@gmail.com>
2018-12-29 14:50:12 -0800

    change script location

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,11 +8,10 @@ import SEO from '../components/seo'
 const IndexPage = () => (
   <Layout>
     <h1>Target div for script below</h1>
-    <div dangerouslySetInnerHTML={{
-      __html: `
-      <div id='frameTitle' class='embedded-widget-title' style='font-size: 23px; color: #333;font-family:Arial, Helvetica, sans-serif; line-height:24px; padding: 18px 10px 8px; text-align: center; -webkit-box-sizing: border-box; -moz-box-sizing: border-box; box-sizing: border-box;'>Book Now</div><div class="vagaro"></div>
-      `
-    }}/>
+      <div id='frameTitle' className='embedded-widget-title'>
+        Book Now
+      </div>
+      <div className="vagaro"></div>
   </Layout>
 )
 
